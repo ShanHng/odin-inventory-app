8f32837a42d82ab98d60ec7f61a064eaba85a731
Shan <hngshanhern@gmail.com>
2024-10-17 10:07:43 +0800

    Set usePolling to true to enable HMR on WSL2 when using Windows editor

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      // Have to set this for HMR to work when using WSL2 with VSCode in Windows. Note: leads to high CPU utilization. https://vite.dev/config/server-options#server-watch
+      usePolling: true
+    }
+  }
 })
